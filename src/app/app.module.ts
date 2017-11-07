2d2cfcfb94f0fe2bf2c8b3f7e8670316234df293
Dax <limeside@gmail.com>
2017-11-07 15:35:36 -0600

    Add signup container component. :rabbit:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SignupService } from './shared/signup.service';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [SignupService],
   bootstrap: [AppComponent]
